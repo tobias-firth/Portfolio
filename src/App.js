566a7b127d50b1d90eb7a0236c41786df82d8667
Tobias Firth <tobiasfirth@Tobiass-Mac-mini.local>
2023-10-01 12:47:34 +0100

    fix typo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import Footer from ".componnets/Footer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
