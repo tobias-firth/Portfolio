8fb5cb88ed7880dec8e227b9fc05d355eaeeb459
Tobias Firth <tobiasfirth@Tobiass-Mac-mini.local>
2023-10-04 13:14:04 +0100

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,16 @@ function Footer() {
         <div id="footer" className=" w-full items-center justify-between px-2 py-3 bg-[#025]">
             <ul className="flex flex-col md:flex-row list-none md:ml-auto">
               <li className="nav-item">
-                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="https://github.com/Sam010000101/to-go-list">Github</a>
+                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6]" href="https://github.com/Sam010000101/to-go-list">Footer 1</a>
               </li>
               <li className="nav-item">
-                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="#nav">Back to Top</a>
+                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6]" href="https://github.com/Sam010000101/to-go-list">Footer 2</a>
               </li>
               <li className="nav-item">
-                <p className="px-3 py-2 flex items-center text-l font-itim text-xl leading-snug text-[#add8e6]">A Stars in a Jar Creation, 2023</p>
+                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6]" href="https://github.com/Sam010000101/to-go-list">Footer 3</a>
+              </li>
+              <li className="nav-item">
+                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6]" href="#nav">Back to Top</a>
               </li>
             </ul>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar({scrollToTop}) {
     const [navbarOpen, setNavbarOpen] = React.useState(false);
     return (
       <div>
-        <nav ref={scrollToTop} id="nav" className="relative w-full flex flex-wrap items-center justify-between px-2 py-3 bg-[#025] z-10">
+        <nav ref={scrollToTop} id="nav" className=" w-full flex flex-wrap items-center justify-between px-2 py-3 bg-[#025] z-10">
           <div className="container px-3 mx-auto flex flex-wrap items-center justify-between">
             <div className="w-full relative flex justify-between md:w-auto md:static md:block md:justify-start">
               <a className="text-xl md:text-l font-bold inline-block mr-3 py-2 whitespace-nowrap font-itim text-[#add8e6]" href="/">Tobias Firth</a>
@@ -18,16 +18,16 @@ function Navbar({scrollToTop}) {
             >
               <ul className="flex flex-col md:flex-row list-none md:ml-auto">
                 <li className="nav-item">
-                  <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text- font-itim lowercase leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="#schedule">About</a>
+                  <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text- font-itim lowercase leading-snug text-[#add8e6]" href="#about">About</a>
                 </li>
                 <li className="nav-item">
-                  <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-m font-itim lowercase leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="#currency">Projects</a>
+                  <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-m font-itim lowercase leading-snug text-[#add8e6]" href="#projects">Projects</a>
                 </li>
                 <li className="nav-item">
-                  <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-m font-itim lowercase leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="#weather">Skills</a>
+                  <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-m font-itim lowercase leading-snug text-[#add8e6]" href="#skills">Skills</a>
                 </li>
                 <li className="nav-item">
-                  <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-m font-itim lowercase leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="#contact">Contact</a>
+                  <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-m font-itim lowercase leading-snug text-[#add8e6]" href="#contact">Contact</a>
                 </li>
               </ul>
             </div>
